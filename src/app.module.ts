841de3ad6eb052398a72a5d48a6b06b04b34d890
mmemaa <deen.memic@scv.si>
2026-01-04 12:01:35 +0100

    feat: Configure app modules for tickets, SLA, and notifications

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TicketsModule } from './tickets/tickets.module';
+import { SLAModule } from './sla/sla.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ScheduleModule.forRoot(),
+    UsersModule,
+    TicketsModule,
+    SLAModule,
+    NotificationsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
